f4730351009d1db8a5c51ee845f613bfff1db287
Anarbol Martinez <48232938+Ugin99@users.noreply.github.com>
2021-09-13 21:10:10 -0700

    Update Story.c

--- The Arizona Trail/Story.c
+++ The Arizona Trail/Story.c
@@ -1,34 +1,56 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
 //variables
 
-char x = first
-char y = second
-char z = third
+char name[10];
+char name1[10];
+char name2[10];
+char place[10];
 
 int main()
 {
     //Here I am introducing the game to the players
-    printf("Welcome to the Arizona Trail. Here you will type in your choices and those choices will affect the outcome of your story\n");
+    printf("Welcome to The Arizona Trail. Here you will type in your choices and those choices will affect the outcome of your story\n");
 
     // Here I grab the main players name
-    char name[10];
     printf("\nWhat's your name?\n");
     fgets(name, 10, stdin);
     printf("Hi there, %s\n", name);
 
     // Next I grab their 2 companions
-    char name1[20];
     printf("\nNext, name 2 people you want to bring on your journey. Who is the first companion?\n");
-    fgets(name1, 20, stdin);
-    printf("You have chosen %s!\n", name1);
+    fgets(name1, 10, stdin);
+    printf("You have chosen %s\n", name1);
 
-    char name2[20];
     printf("\nAnd the second companion?\n");
-    fgets(name2, 20, stdin);
-    printf("You have chosen %s!\n", name2);
+    fgets(name2, 10, stdin);
+    printf("You have chosen %s\n", name2);
 
+    bool i = false;
+    char placeTempe[10] = "Tempe";
+    char placeAvondale[10] = "Avondale";
 
+    while (i == false) {
+        printf("\nNow you must choose where to start your journey, Tempe or Avondale:");
+        fgets(place, 10, stdin);
+
+    if (strcmp(place, placeTempe) == 0)
+    {
+        printf("\nYou are heading to Tempe");
+        i = true;
+    }
+    else if (strcmp(place, placeAvondale) == 0)
+    {
+        printf("\nYou are going to Avondale");
+        i = true;
+    }
+    else {
+        printf("\nYou fucked up hoe try to spell correctly");
+    }
+}
     return 0;
 }
+
 
